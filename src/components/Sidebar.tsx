70972d970308d0442b2132589a2c0dbb0f8fda0c
narchok <jabin.julian@gmail.com>
2023-06-21 23:02:29 +0200

    refactor(sidebar): update elements order

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -107,25 +107,6 @@ const Sidebar: React.FC<{ children: ReactElement }> = ({ children }) => {
           </Link>
         </li>
       )}
-      <li>
-        <Link href="/infos">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          Infos
-        </Link>
-      </li>
       {isLoggedIn && candidature && (
         <li>
           <Link href={`/cv/${candidature.candidatureId}`}>
@@ -150,6 +131,25 @@ const Sidebar: React.FC<{ children: ReactElement }> = ({ children }) => {
           </Link>
         </li>
       )}
+      <li>
+        <Link href="/infos">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          Infos
+        </Link>
+      </li>
 
       {isLoggedIn && (
         <li className="mt-auto">
