29971ddaf8f23c69f3997a79e039e614baa05793
narchok <jabin.julian@gmail.com>
2023-06-23 23:06:15 +0200

    refactor(signin): responsive for signin page

--- src/pages/auth/email-signin.tsx
+++ src/pages/auth/email-signin.tsx
@@ -6,9 +6,9 @@ import Image from 'next/image'
 export default function SignIn() {
   const [email, setMail] = useState('')
   return (
-    <div className="flex relative justify-center items-center top-1/2">
-      <div className="flex relative justify-center items-center top-1/2 ">
-        <div className="form-control max-w-full space-y-2 w-[25rem]">
+    <div className="flex justify-center items-center h-screen">
+      <div className="border rounded-lg p-4 flex-col">
+        <div className="form-control max-w-full space-y-2">
           <label className="label">
             <span className="label-text text-xl font-bold text-mc">Adresse email</span>
           </label>
@@ -21,8 +21,7 @@ export default function SignIn() {
             className="input input-bordered w-full "
           />
           <p className="text-gray-500">
-            Vous allez être notifié par e-mail afin de valider la connexion à votre
-            compte.
+            Vous allez être notifié par email afin de valider la connexion à votre compte.
           </p>
           <button
             className="btn btn-primary"
@@ -32,14 +31,15 @@ export default function SignIn() {
             Se connecter
           </button>
         </div>
-
-        <Image
-          src={mc}
-          className={'max-w-xs'}
-          alt="Logo de Miage Connection"
-          loading="eager"
-          placeholder="blur"
-        />
+        <div className="flex justify-center">
+          <Image
+            src={mc}
+            className={'max-w-xs'}
+            alt="Logo de Miage Connection"
+            loading="eager"
+            placeholder="blur"
+          />
+        </div>
       </div>
     </div>
   )
