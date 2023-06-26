08771d8e0f333d25aaaa321d9b073038745694c2
narchok <jabin.julian@gmail.com>
2023-06-26 16:56:52 +0200

    fix(candidature): remove unused field

---
+++ prisma/migrations/20230626143529_remove_unused_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `cityInfo` on the `Candidature` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Candidature" DROP COLUMN "cityInfo";
